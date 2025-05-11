ba85a3b7f28779f2461fdd9696e23be1e36355f4
tutsguides <alisktl@yandex.ru>
2025-05-11 14:30:12 +0500

    binary search on predicate

---
+++ search-algorithms/binary-search-on-arbitrary-predicate-1/cpp/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class BinarySearch {
+private:
+    bool pred(const int num) {
+        if (num >= 12) return true;
+        return false;
+    }
+
+public:
+    int search(const vector<int> &nums) {
+        int n = int(nums.size());
+        int low = -1;
+        int high = n;
+
+        while (high > low + 1) {
+            int mid = low + (high - low) / 2;
+
+            if (pred(nums[mid])) {
+                high = mid;
+            } else {
+                low = mid;
+            }
+        }
+
+        return high;
+    }
+};
+
+int main() {
+    vector nums = {2, 4, 9, 12, 12, 12, 43, 78, 85, 89};
+
+    BinarySearch binarySearch;
+    cout << binarySearch.search(nums) << endl;
+}
---
+++ search-algorithms/binary-search-on-arbitrary-predicate-3/java/BinarySearch.java
@@ -0,0 +1,29 @@
+public class BinarySearch {
+    static boolean pred(int num) {
+        if (num > 12) return true;
+        return false;
+    }
+
+    static int search(int[] nums) {
+        int low = -1;
+        int high = nums.length;
+
+        while (high > low + 1) {
+            int mid = low + (high - low) / 2;
+
+            if (pred(nums[mid])) {
+                high = mid;
+            } else {
+                low = mid;
+            }
+        }
+
+        return low;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 4, 9, 12, 12, 12, 43, 78, 85, 89};
+
+        System.out.println(search(nums));
+    }
+}
